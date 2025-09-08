c26d8681d058c987812f0255095fb9507ff8ab52
annaderosis <162120430+annamaldotti@users.noreply.github.com>
2025-09-08 09:04:24 -0300

    a

---
+++ main.js
@@ -0,0 +1,23 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+
+botoes [0].onclik = diminuiTamanho;
+botoes [1].onclik = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha >1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho() {
+    if (tamanhoSenha <20) {
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
