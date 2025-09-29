e9c8b3da735adf2d940ae5469d8d3b737b0ad5e5
annaderosis <162120430+annamaldotti@users.noreply.github.com>
2025-09-29 08:49:35 -0300

    a

--- main.js
+++ main.js
@@ -52,4 +52,12 @@ function geraSenha () {
     if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
+
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i ++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha =  senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
